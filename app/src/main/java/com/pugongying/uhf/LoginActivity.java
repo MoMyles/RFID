8426bda409a6513ae0b64554e5914c29cc8a1b24
momyles <maogd_0916@163.com>
2018-06-25 09:48:04 +0800

    登录界面搭建

---
+++ app/src/main/java/com/pugongying/uhf/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.pugongying.uhf;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
