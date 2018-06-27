788b66564ca75abc2140e8bffcdeec6070b06363
momyles <maogd_0916@163.com>
2018-06-27 10:22:16 +0800

    登录完成

--- app/src/main/java/com/pugongying/uhf/LoginActivity.java
+++ app/src/main/java/com/pugongying/uhf/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.pugongying.uhf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -52,9 +55,9 @@ public class LoginActivity extends AppCompatActivity {
                             if (obj != null) {
                                 int succ = obj.getIntValue("succ");
                                 if (succ == 1) {
-
+                                    startActivity(new Intent(LoginActivity.this, IndexActivity.class));
                                 } else {
-
+                                    Toast.makeText(getApplicationContext(), obj.getString("msg"), Toast.LENGTH_SHORT).show();
                                 }
                             }
                         }
